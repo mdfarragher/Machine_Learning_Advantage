515dfe0b35632a6da092fb78970e98e8cf832f8b
Mark Farragher <mfarragher@gmail.com>
2018-11-13 17:57:29 +0100

    In module 2, updated lesson 2 and added starter projects

--- module_3/reference/lesson_2/Program.cs
+++ module_3/reference/lesson_2/Program.cs
@@ -26,6 +26,10 @@ namespace ml_csharp_lesson2
             var training_labels = DataUtil.LoadBinary<float>("train_labels.bin", 60000, 10);
             var test_labels = DataUtil.LoadBinary<float>("test_labels.bin", 10000, 10);
 
+            // report results
+            Console.WriteLine($"{training_data.GetLength(0)} training digits loaded");
+            Console.WriteLine($"{test_data.GetLength(0)} test digits loaded");
+
             // build features and labels
             var features = NetUtil.Var(new int[] { 28, 28 }, DataType.Float);
             var labels = NetUtil.Var(new int[] { 10 }, DataType.Float);
@@ -36,11 +40,11 @@ namespace ml_csharp_lesson2
                 .Dense(10)
                 .ToNetwork();
 
-            // set up the loss function and the accuracy function
+            // set up the loss function and the classification error function
             var lossFunc = CNTKLib.CrossEntropyWithSoftmax(network.Output, labels);
             var errorFunc = CNTKLib.ClassificationError(network.Output, labels);
 
-            // set up a trainer that uses an RMS algorithm
+            // set up a trainer that uses the RMSProp algorithm
             var learner = network.GetRMSPropLearner(
                 learningRateSchedule: 0.99,
                 gamma: 0.95,
@@ -50,11 +54,16 @@ namespace ml_csharp_lesson2
                 min: 0.5
             );
 
+            // set up a trainer and an evaluator
             var trainer = network.GetTrainer(learner, lossFunc, errorFunc);
             var evaluator = network.GetEvaluator(errorFunc);
 
-            var maxEpochs = 10;
+            // declare some variables
+            var maxEpochs = 20;
             var batchSize = 128;
+            var loss = 0.0;
+            var error = 0.0;
+            var batchCount = 0;
 
             // train the network during several epochs
             Console.WriteLine("Training the neural network....");
@@ -63,6 +72,9 @@ namespace ml_csharp_lesson2
                 Console.Write($"Training epoch {epoch+1}/{maxEpochs}... ");
 
                 // train the network using random batches
+                loss = 0.0;
+                error = 0.0;
+                batchCount = 0;
                 training_data.Index().Shuffle().Batch(batchSize, (indices, begin, end) =>
                     {
                         // get the current batch
@@ -77,35 +89,42 @@ namespace ml_csharp_lesson2
                             },
                             false
                         );
-                        if (begin == 0)
-                           Console.WriteLine($"loss: {result.Loss}");
+                        loss += result.Loss;
+                        error += result.Evaluation;
+                        batchCount++;
                     });
-            }
 
-            var accuracy= 0.0;
-            var numBatches = 0;
+                // show results
+                loss /= batchCount;
+                error /= batchCount;
+                Console.WriteLine($"loss: {loss}, error: {error}");
+            }
 
             // test the network using batches
             Console.WriteLine("Testing the neural network....");
+            error = 0.0;
+            batchCount = 0;
             test_data.Batch(batchSize, (data, begin, end) =>
                 {
-                    // get the current batch
+                    // get the current batch for testing 
                     var featureBatch = features.GetBatch(test_data, begin, end);
                     var labelBatch = labels.GetBatch(test_labels, begin, end);
 
                     // test the network on the batch
-                    accuracy += evaluator.TestBatch(
+                    error += evaluator.TestBatch(
                         new[] {
                             (features, featureBatch),
                             (labels,  labelBatch)
                         }
                     );
-                    numBatches++;
+                    batchCount++;
                 }
             );
 
             // show results
-            Console.WriteLine($"Final classification error on test data: {accuracy / numBatches}");
+            error /= batchCount;
+            Console.WriteLine($"Final classification error on test data: {100 * error:0.00}%");
+            Console.WriteLine($"Final accuracy on test data: {1 - error:0.00}");
 
             Console.ReadLine();
         }
---
+++ module_3/starter/lesson_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CNTK;
+using Pensar;
+
+namespace ml_csharp_lesson2
+{
+    class Program
+    {
+        /// <summary>
+        /// The main entry point of the application.
+        /// </summary>
+        /// <param name="args">The command line arguments.</param>
+        static void Main(string[] args)
+        {
+            // ******************
+            // ADD YOUR CODE HERE
+            // ******************
+
+            Console.ReadLine();
+        }
+    }
+}
