9f82b31d1d6a9dbb3540d4c54c62e1a9c7fd0108
Mark Farragher <mfarragher@gmail.com>
2018-12-12 19:31:33 +0100

    Added module 7 lesson 2

---
+++ module_7/starter/lesson_2/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CNTK;
+using Pensar;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+
+namespace ml_csharp_lesson2
+{
+    /// <summary>
+    /// The Plot class encapsulates a plotting window. 
+    /// </summary>
+    class Plot : System.Windows.Window
+    {
+        /// <summary>
+        /// Construct a new instance of the class.
+        /// </summary>
+        /// <param name="results">The data to plot.</param>
+        public Plot(List<List<double>> results)
+        {
+            // set up a new plot
+            var plotModel = new PlotModel();
+            plotModel.Title = "Training and Validation error";
+
+            // add x and y axis
+            plotModel.Axes.Add(
+                new LinearAxis()
+                {
+                    Position = AxisPosition.Left,
+                    Title = "Classification Error"
+                }
+            );
+            plotModel.Axes.Add(
+                new LinearAxis()
+                {
+                    Position = AxisPosition.Bottom,
+                    Title = "Epochs"
+                }
+            );
+
+            // add data series
+            var labels = new string[] { "Training", "Validation" };
+            var colors = new OxyColor[] { OxyColors.Blue, OxyColors.Green };
+            for (int row = 0; row < results.Count; row++)
+            {
+                var lineSeries = new LineSeries
+                {
+                    ItemsSource = results[row].Select((value, index) => new DataPoint(index, value)),
+                    Title = labels[row],
+                    Color = colors[row]
+                };
+                plotModel.Series.Add(lineSeries);
+            }
+
+            // wrap up
+            Title = "Chart";
+            Content = new OxyPlot.Wpf.PlotView
+            {
+                Model = plotModel
+            };
+        }
+    }
+
+    /// <summary>
+    /// The MyTrainingEngine class represents the training engine to use in this assignment.
+    /// </summary>
+    class MyTrainingEngine: TrainingEngine
+    {
+        /// <summary>
+        /// Create the feature variable.
+        /// </summary>
+        /// <returns>The feature variable to use.</returns>
+        protected override CNTK.Variable CreateFeatureVariable()
+        {
+            return NetUtil.Var(new int[] { 1 }, CNTK.DataType.Float);
+        }
+
+        /// <summary>
+        /// Create the label variable.
+        /// </summary>
+        /// <returns>The label variable to use.</returns>
+        protected override CNTK.Variable CreateLabelVariable()
+        {
+            var axis = new List<CNTK.Axis>() { CNTK.Axis.DefaultBatchAxis() };
+            return NetUtil.Var(new int[] { 1 }, CNTK.DataType.Float, dynamicAxes: axis);
+        }
+
+        /// <summary>
+        /// Create the model.
+        /// </summary>
+        /// <param name="features">The input feature to build the model on.</param>
+        /// <returns>The completed model to use.</returns>
+        protected override CNTK.Function CreateModel(CNTK.Variable features)
+        {
+            // ******************
+            // ADD YOUR CODE HERE
+            // ******************
+
+            return null; // remove this when done!
+        }
+    }
+
+    /// <summary>
+    /// The main application class.
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// The main entry point. 
+        /// </summary>
+        /// <param name="args">The command line arguments.</param>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            // unpack archive
+            Console.WriteLine("Unpacking archive...");
+            if (!System.IO.File.Exists("x_train_imdb.bin"))
+            {
+                DataUtil.Unzip(@"..\..\..\..\..\imdb_data.zip", ".");
+            }
+
+            // load training and test data
+            Console.WriteLine("Loading data files...");
+            var trainingPartitionX = DataUtil.LoadBinary<float>("x_train_imdb.bin", 25000, 500);
+            var trainingPartitionY = DataUtil.LoadBinary<float>("y_train_imdb.bin", 25000);
+            var testPartitionX = DataUtil.LoadBinary<float>("x_test_imdb.bin", 25000, 500);
+            var testPartitionY = DataUtil.LoadBinary<float>("y_test_imdb.bin", 25000);
+
+            // reserve the final 20% of training data for validation
+            var pivot = (int)(trainingPartitionX.Length * 0.8);
+            var trainingFeatures = trainingPartitionX.Skip(pivot).ToArray();
+            var trainingLabels = trainingPartitionY.Skip(pivot).ToArray();
+            var validationFeatures = trainingPartitionX.Take(pivot).ToArray();
+            var validationLabels = trainingPartitionY.Take(pivot).ToArray();
+
+            // ******************
+            // ADD YOUR CODE HERE
+            // ******************
+
+            Console.ReadLine();
+        }
+    }
+}
