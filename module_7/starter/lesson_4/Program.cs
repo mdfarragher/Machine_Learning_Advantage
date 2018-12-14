d564cd19b1e73b69bda8f38c1ba339f0e649534d
Mark Farragher <mfarragher@gmail.com>
2018-12-14 19:14:41 +0100

    Added lesson 4 to module 7

---
+++ module_7/starter/lesson_4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenCvSharp;
+using Pensar;
+
+namespace ml_csharp_lesson4
+{
+    /// <summary>
+    /// The application class.
+    /// </summary>
+    class Program
+    {
+        // the number of latent dimensions to use in the generator
+        static readonly int latentDimensions = 32;
+
+        // the image dimensions and number of color channels
+        static readonly int imageHeight = 32;
+        static readonly int imageWidth = 32;
+        static readonly int channels = 3;
+
+        /// <summary>
+        /// The main application entry point.
+        /// </summary>
+        /// <param name="args">The command line arguments</param>
+        static void Main(string[] args)
+        {
+            // ******************
+            // ADD YOUR CODE HERE
+            // ******************
+        }
+    }
+}
